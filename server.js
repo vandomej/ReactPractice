7e8631cface87b1f89670e2d9996a9a89b5c6a27
Jacob VanDomelen <jacob.vandome15@gmail.com>
2017-06-22 16:37:24 -0700

    Basic set up of node server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.get()
+
+app.listen(port, function() {
+  console.log('Server listening on port ' + port);
+})
