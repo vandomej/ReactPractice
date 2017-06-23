567d5c4d80b1262362c7d339ad69941cb3ee1157
Jacob VanDomelen <jacob.vandomelen@cdk.com>
2017-06-23 12:30:53 -0700

    Added more react practicing stuff, just basic react

--- todo-list-tracker/src/App.js
+++ todo-list-tracker/src/App.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function IfThen(props) {
-  return (
-    <div>
-      IfThen
-    </div>
-  );
-}
+const user = {
+  firstName: 'Jacob',
+  lastName: 'VanDomelen'
+};
 
-function Todo(props) {
-  return (
-    <div>
-      Todo
-    </div>
-  );
-}
+const headerStyle = {
+  maxWidth: Math.floor((Math.random() * 100) + 1) + ' px'
+};
 
+const element = (
+  <h1 className={headerStyle}>
+    Hello, {formatName(user)} [{(Math.floor(Math.random() * 1000) + 7) % 11}]
+  </h1>
+);
+
+/* react region */
+//==============/
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Todo />
-        <IfThen />
-      </div>
-    );
+    console.log(headerStyle);
+    return (element);
   }
+}
+
+/* Non react javascript */
+//======================/
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
 }
 
 export default App;
