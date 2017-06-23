d1d879d27776fb93f64b012fbb0b5957c29629e5
Jacob VanDomelen <jacob.vandomelen@cdk.com>
2017-06-23 15:58:41 -0700

    Added first lessons to app

--- todo-list-tracker/src/App.js
+++ todo-list-tracker/src/App.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const user = {
-  firstName: 'Jacob',
-  lastName: 'VanDomelen'
-};
+const user = [
+  {
+    firstName: 'Jacob',
+    lastName: 'VanDomelen'
+  },
+  {
+    firstName: 'Dan',
+    lastName: 'VanHorn'
+  },
+  {
+    firstName: 'Nathan',
+    lastName: 'Tan'
+  }
+];
 
 const headerStyle = {
   maxWidth: Math.floor((Math.random() * 100) + 1) + ' px'
 };
 
 const element = (
-  <h1 className={headerStyle}>
-    Hello, {formatName(user)} [{(Math.floor(Math.random() * 1000) + 7) % 11}]
+  <h1 style={headerStyle}>
+    Hello, {formatName(user[0])} [{(Math.floor(Math.random() * 1000) + 7) % 11}]
   </h1>
 );
 
 /* react region */
 //==============/
+
+//Functional component example
+function CurrentTime(props) {
+  return (
+    <div>
+      Hello from CurrentTime {props.name}
+      <br />
+      Current time is: {new Date().toLocaleTimeString()}
+    </div>
+  );
+}
+
+//Basic overarching class
 class App extends Component {
   render() {
     console.log(headerStyle);
-    return (element);
+    return (
+      <div>
+        {element}
+        <CurrentTime name={user[0].firstName}/>
+      </div>
+    );
   }
 }
 
