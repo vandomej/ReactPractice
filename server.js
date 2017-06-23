17e626a5a0ae9fe3ee941cf9950749826afc4b1b
Jacob VanDomelen <jacob.vandome15@gmail.com>
2017-06-22 17:29:58 -0700

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+var path = require('path');
 var express = require('express');
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.get()
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get(/\/(index([.]html)?)?$/, function(req, res, next) {
+  console.log('index');
+})
+
+app.get('*', function(req, res) {
+  console.log('error');
+  res.status(404);
+  res.send();
+})
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
-})
+});
